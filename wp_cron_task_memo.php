531bb263025c0e0db777bedb423f35db22a1658d
Loïc <loic.laurent@outlook.com>
2020-11-15 11:51:21 +0100

    init plugin

---
+++ wp_cron_task_memo.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: WP cron task Memo
+ * Description: Memo pour créer des taches cron dans WP
+ * Version: 1.0
+ * Author: Loïc Laurent
+ * Author URI: https://www.loiclaurent.com
+*/
+
+class demoCron {
+
+    public function __construct() {
+
+        
+    }
+}
