78adde68174ed84dcd780510ebd7bb07b92e0fe2
Loïc <loic.laurent@outlook.com>
2020-12-22 14:52:32 +0100

    cron

--- wp_cron_task_memo.php
+++ wp_cron_task_memo.php
@@ -10,8 +10,43 @@
 
 class demoCron {
 
+    protected $adminEmail;
+
     public function __construct() {
 
-        
+        // cron task
+        add_action('my_cron_task', array($this, 'cronTask'));
+
+        // activation du plugin
+        register_activation_hook(__FILE__, array($this, 'activation'));
+
+        // désactivation du plugin
+        register_deactivation_hook(__FILE__, array($this, 'desactivation'));
+
+        $this->adminEmail = get_option( 'admin_email', '' );
+    }
+
+    public function activation() {
+        // register cron task
+        wp_mail( 'crontask@demo.com', 'Activation du cron', 'Activation du cron' );
+        if (!wp_next_scheduled('my_cron_task')) {
+            wp_schedule_event( time(), 'hourly', 'my_cron_task' );
+        }
+    }
+    public function desactivation() {
+        // de-register cron task
+        wp_mail( 'crontask@demo.com', 'Déctivation du cron', 'Déctivation du cron' );
+        if (wp_next_scheduled('mp_cron_import')) {
+            $timeStamp = wp_next_scheduled('my_cron_task');
+            wp_unschedule_event( $timeStamp, 'my_cron_task');
+        }
+    }
+
+    public function cronTask() {
+
+        // Do something
+        wp_mail( 'crontask@demo.com', 'Passage du cron', 'Passage du cron' );
     }
 }
+
+new demoCron;
